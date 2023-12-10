0555d6651d693a00ce1ad94862c7a37cc286f2af
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-10 09:43:08 -0500

    adding confetti when you win using the confetti library

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
 
   return (
     <main className="App">
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p>Roll untill all dice are the same. Click each to freeze it at its current value between rolls.</p>
       <div  className='dices-container'>
