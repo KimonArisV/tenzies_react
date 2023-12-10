ee892121aece1a15ba2a4f5f176c2606c8a2d99d
Kimon Vogt <kvogt@g.harvard.edu>
2023-12-10 07:45:25 -0500

    alter the functions used such that my elements of the array are objects with isHeld value and i do not change the ones where their value is true. I used ternary as well as .map and anonymous functions and State ultility. I also used the idea that () is needed when you return an object. Lastly onClick needs a anonymous function instead of a function call since it will call it only when the button will be clicked. Spead systnax was also used ...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import Die from "./assets/components/Die";
 import React from "react";
+//import {nanoid} from "nanoid";
 
 
 function App() {
   const rollDiceFunc = ()=> Math.floor(6*Math.random());
   const initialDiceArray =Array.from({ length: 10 },rollDiceFunc);
-  const [dice,setDice] = React.useState(initialDiceArray);
-
+  const initialState = initialDiceArray.map(  element=>(  {"value" : element, "isHeld" : false} ) );
+  const [dice,setDice] = React.useState(initialState);
+  
   //create the tags to render the dices
-  const diceArraysTags = dice.map(oneDiceValue=><Die value={oneDiceValue} />)
+  const diceArraysTags = dice.map(oneDice=><Die value={oneDice.value} />);
 
-  //console.log(initialDiceNum())
+  //console.log(rollDiceFunc())
+  //create function that will be used to change the values of the dices id the isHeld is false
+  //used ternary to return in a single line. 
+  //Because we use .map this function will be applied to each of the elements of the array individually
+  //and each element is an object, therefore we need to open it up and then overwirte the value of it
+  const changeValuesFunc = element=> !element.isHeld ? { ...element, "value": rollDiceFunc()} : element;
+
   return (
     <main className="App">
       <div  className='dices-container'>
         {diceArraysTags}
       </div>
-      <button className="rolldice_button" onClick={()=>setDice(prevDice => prevDice.map(rollDiceFunc))}>Roll</button>
+      <button className="rolldice_button" onClick={()=>setDice(prevArray=>prevArray.map(changeValuesFunc))}>Roll</button>
     </main>
   );
 }
